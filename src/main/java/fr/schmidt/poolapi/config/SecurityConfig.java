00375f5046a2d7fa369621bfceeb2234dfd02801
Thomas Schmidt <thomas.michel.schmidt@gmail.com>
2026-04-21 10:56:16 +0200

    upgrade to hostinger VPS starting

--- src/main/java/fr/schmidt/poolapi/config/SecurityConfig.java
+++ src/main/java/fr/schmidt/poolapi/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500", "https://pool-front.onrender.com/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500", "https://pool-front.thomasschmidt.fr"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
